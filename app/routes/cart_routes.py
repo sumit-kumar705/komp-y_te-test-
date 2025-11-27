a588ed0afc8e65a30ef26b49a10ec2aa19dc1186
sumit-kumar705 <sumitsharma9032@gmail.com>
2025-11-27 12:13:05 +0530

    added user auth to cart

--- app/routes/cart_routes.py
+++ app/routes/cart_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.services.cart_services import add_to_cart, remove_from_cart, get_user_cart
 from app.utils.response import success_response, error_response
 
@@ -6,13 +7,17 @@ from app.utils.response import success_response, error_response
 cart_bp = Blueprint("cart", __name__)
 
 @cart_bp.route("/", methods=["GET"])
+@jwt_required()
 def view_cart():
-    user_id = request.args.get("user_id")
+    user_id = get_jwt_identity()
+    #user_id = request.args.get("user_id")
     cart_items = get_user_cart(user_id)
     return success_response(cart_items)
 
 @cart_bp.route("/add", methods=["POST"])
+@jwt_required()
 def add_item():
+    user_id = get_jwt_identity()
     data = request.get_json() or {}
     item = add_to_cart(data)
     if item:
